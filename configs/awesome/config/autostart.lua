45a545c1f7ebeb66d4c1f61b3b93bfa9772119d3
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2023-04-07 08:25:30 +0000

    Add 1password to autostart.

--- configs/awesome/config/autostart.lua
+++ configs/awesome/config/autostart.lua
@@ -2,11 +2,13 @@ local awful = require("awful")
 local helpers = require("helpers")
 
 local function autostart_apps()
-	--- Compositor
-	helpers.run.check_if_running("picom", nil,
-		function() awful.spawn("picom", false) end)
-	--- Polkit Agent
-	helpers.run.run_once_ps("lxpolkit", "/usr/bin/lxpolkit")
+    --- Compositor
+    helpers.run.check_if_running("picom", nil,
+                                 function() awful.spawn("picom", false) end)
+    --- Polkit Agent
+    helpers.run.run_once_ps("lxpolkit", "/usr/bin/lxpolkit")
+    --- 1password
+    helpers.run.run_once_ps("1password", "/usr/sbin/1password --silent")
 end
 
 autostart_apps()
